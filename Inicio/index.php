8c98d1af045b30d846ea0755b2ba7bd47bcfb373
karina sanchez <karoty_25@hotmail.com>
2015-07-20 14:50:26 -0500

    prueba

---
+++ Inicio/index.php
@@ -0,0 +1,12 @@
+        <?php
+        error_reporting(0);
+//         $url = parse_url(getenv("CLEARDB_DATABASE_URL "));
+        $server = "us-cdbr-iron-east-02.cleardb.net";
+        $username = "b93dd65416822b";
+        $password = "89003cb9";
+        $db = substr("heroku_5a543f116c54fb0", 1);
+        $conn = new mysqli($server, $username, $password, $db);
+		if ($conn->connect_error)
+		    die($conn->connect_error);
+		else
+		   echo 'Conexion exitosa' . '<br>' . '<br>';
