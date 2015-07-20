a7fac5d580010c1a69cc490f9b70cdc6dc333036
karina sanchez <karoty_25@hotmail.com>
2015-07-20 15:15:47 -0500

    prueba

--- Inicio/index.php
+++ Inicio/index.php
@@ -4,7 +4,7 @@
         $server = "us-cdbr-iron-east-02.cleardb.net";
         $username = "b93dd65416822b";
         $password = "89003cb9";
-        $db = substr("heroku_5a543f116c54fb0", 1);
+        $db = "heroku_5a543f116c54fb0";
         $conn = new mysqli($server, $username, $password, $db);
 		if ($conn->connect_error)
 		    die($conn->connect_error);
